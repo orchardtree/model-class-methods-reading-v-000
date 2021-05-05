0e3f3c961caa5e850cda37c81ef42bf6c7eb323c
Jesse Wenger <wengerj@gmail.com>
2021-05-05 23:03:39 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,9 @@ class PostsController < ApplicationController
 
     # filter the @posts list based on user input
     if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+    if !params[:author].blank?
       @posts = Post.where(author: params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
