7445d46c4f39d340301a9ae577c1ce2c0d471251
Jesse Wenger <wengerj@gmail.com>
2021-05-05 23:02:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,11 @@ class Post < ActiveRecord::Base
   belongs_to :author
 
   #put new code here
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
 
+  
   private
 
   def is_title_case
